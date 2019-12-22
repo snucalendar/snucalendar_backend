700f0586a5fbca57df0e1811f9053ed01d06d11f
jangsus1 <min99101511@naver.com>
2019-12-22 21:58:30 +0900

    url

--- backend/waffle/urls.py
+++ backend/waffle/urls.py
@@ -4,7 +4,17 @@ from . import views
 urlpatterns = [
     path('token/', views.token, name='token'),
     path('login/', views.login, name='login'),
-    path('logout/', views.signup, name='logout'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('userinfo/', views.userinfo, name='userinfo'),
+    path('calendar/month/<int:year>/<int:month>/', views.calendarMonth, name='calendarMonth'),
+    path('calendar/date/<int:year>/<int:month>/<int:date>/', views.calendarDate, name='calendarDate'),
     
-
+    path('api/events/', views.events),
+    path('api/events/<int:id>/', views.event),
+    path('api/events/<int:id>/participate/'),
+    path('api/events/<int:id>/rating'),
+    path('api/search/<str:keyword>'),
+    path()
+    
 ]
