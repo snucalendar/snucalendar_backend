1f955e4be8164c5acd1027e57bcfd15f525a9514
jangsus1 <min99101511@naver.com>
2020-03-14 17:31:41 +0900

    admin 등록

--- backend/waffle/models.py
+++ backend/waffle/models.py
@@ -32,7 +32,7 @@ class EventComment(models.Model):
 class QnA(models.Model):
     question = models.TextField()
     answer = models.TextField()
-    completed = models.BooleanField(null=, blank=True)
+    completed = models.BooleanField(null=True, blank=True)
     question_author = models.ForeignKey(get_user_model(), on_delete = models.CASCADE, related_name = 'question')
     answer_author = models.ForeignKey(get_user_model(), on_delete = models.CASCADE, related_name = 'answer', null = True, blank=True)
     event = models.ForeignKey(Event, on_delete = models.CASCADE, related_name = 'QnA', db_index=True)
